730e04c6c39c8943859b282a58e7c2957587f461
sofie-kirk22 <133001261+sofie-kirk22@users.noreply.github.com>
2026-09-07 15:53:46 +0200

    added extra tag.

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -33,6 +33,7 @@ export default function Projects() {
                         }
                         tags={[
                             "C#",
+                            "ASP.NET",
                             "TypeScript",
                             "React",
                             "NextJS",
